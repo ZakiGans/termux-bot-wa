e0a02455fe96d19053b51acdced03f67604dbae1
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-17 21:51:57 +0700

    Add files via upload

---
+++ tambahan.js
@@ -0,0 +1,9 @@
+module.exports = {
+	halo: "HALO JUGA ZEYENKK* \n\nketik *${prefix}menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	hai: "HAI JUGAA!*\n\n ketik *${prefix}menumenu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	ass: "WAALAIKUMSALAM*\n\n ketik *${prefix}menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	bro: "EUUUYYYY!!!* \n\nketik *${prefix}menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	p: "pa pe pa pe pap pap*\n\n ketik *${prefix}menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	test: "*satu dua dicoba*\n\n ketik *${prefix}menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	sbr: "SABAR BOII LAGI DICARIIN",
+}
