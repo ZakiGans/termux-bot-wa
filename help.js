7370b4929ee5b750e92bb2be900c3c0e1f2c162e
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-17 22:16:18 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,130 @@
+const help = (prefix) => {
+        return `
+╭────────────────
+⛶┃ *༺ HASUFA TEAM ༻*
+┊╭────── 
+┊┠➤ THANX TO ALLAH
+┊┠➤ ARIFI RAZZAQ OFFICIAL
+┠╯
+*┃⛨⧿✧ུNama : EREN BOT*
+*┃⛨⧿✧ུVersi : V5. 0.0*
+*┃⛨⧿✧ུStatus : ACTIVE*
+*┃⛨⧿✧ུPenulis : ARIFI & TEAM*
+*┃⛨⧿✧ུAwalan : 「 ${prefix} 」*
+╰───────────────╗
+╔───────────────╯
+┠─ *❲ ⦁ APA YANG BARU ❳*
+│
+├❏ *Cek : ${prefix}cekup*
+├❏ *riwayat : ${prefix}memobot*
+├❏ *Semua Menu : ${prefix}all*
+├❏ *Thanx To : ${prefix}bycc*
+├❏ *Total : 603 FITUR*
+└────────────────╯
+
+ 
+┌──「 *LIST MENU* 」
+│
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}ownermenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}adminmenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}vipmenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}mediamenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}kerangmenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}makemenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}othermenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}animemenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}nsfwmenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}funmenu*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}wibumenu*
+└ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}hackingmenu*
+
+
+┌──「 *OTHER* 」
+│
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}bugreport*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}info*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}owner*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}request*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}setprefix*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}listblock*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}iklan*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}runtume*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}rules*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}tnc*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}laporbug*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}cekvip*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}daftarvip*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}addvip*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}dellvip*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}snk*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}listpremium*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}donate*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}fitnah*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}totaluser*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}level*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}glass*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}cekup*
+└ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}afk*
+
+
+┌──「 *POPULER* 」
+│
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}apiteks [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}tahta [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}airteks [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}metalteks [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}mlogo [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}ffbaner [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}embun [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}kunciteks [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}say*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}addsay [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}listsay*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}bacot*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}addbacot [text]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}listbacot*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}resetbacot*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}aguse*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}kicktime*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}nobg*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}url2img*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}joox*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}asupan*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}virtex* 
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}cogans*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}cecans* 
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}wame* 
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}animehug*
+└ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}spamcall [8xxxx]*
+
+
+┌──「 *PENGEMBANG* 」
+│
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}leveling [Enable/Disable]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}Anime [1/0]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}nsfw [1/0]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}welcome [1/0]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}public [1/0]*
+└ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}antilinkgroup [1/0]* 
+
+
+┌──「 *HASUFA BUSSINESS* 」
+│
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}kontakdeveloper*
+├ ♡ۣۜۜ፝͜͜͡͡✿➣ *${prefix}daftarmemberselebgram*
+└┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+
+        ▉║█▐▉▉▐▐▍█║▍▉▏▍▍
+        ▉║█▐▉▉▐▐▍█║▍▉▏▍▍   
+                                        
+                               
+          *© ARIFI RAZZAQ*
+          *TEAM CREATOR*        
+          
+                                                        
+                                                                                                                                                    
+*Jangan Lupa Untuk Donasi Agar Bot Selalu Aktif*
+https://saweria.co/Arifirazzaq2001
+`
+}
+exports.help = help
