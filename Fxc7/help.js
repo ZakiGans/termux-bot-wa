1698d8fea69e4a45217470a1e8363b164c4257d2
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-19 05:53:14 +0700

    Add files via upload

---
+++ Fxc7/help.js
@@ -0,0 +1,474 @@
+// menu fitur bot
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { 
+	return `
+	
+\`\`\`Github Me https://github.com/ZakiGans\`\`\`
+
+
+\`\`\`Owmer:wa.me/083144780782\`\`\`
+
+
+\`\`\`NameOwner :ZakiGans\`\`\`
+
+
+
+\`\`\`HARAP BACA NOTE DIBAWAH SEBELUM\`\`\`
+\`\`\`MENGGUNAKAN BOT\`\`\`
+
+╭──────「 *REGULATION ${name}* 」
+┴
+┣⊱  \`\`\`NAMA USER:\`\`\` *${pushname2}*
+┣⊱  \`\`\`VERIVICATION:\`\`\` ✅
+┣⊱  \`\`\`LIMIT:\`\`\` *${limitt} perhari*
+┣⊱  \`\`\`AKTIF:\`\`\` ${kyun(uptime)}
+┣⊱  \`\`\`JAM:\`\`\` *${jam} WIB*
+┣⊱  \`\`\`TANGGAL:\`\`\` *${tanggal}*
+┣⊱  \`\`\`VERSION:\`\`\` *6.5.0*
+┣⊱  \`\`\`USER TERDAFTAR:\`\`\` *${user.length} User*
+┣⊱  ❌ *SPAM*
+┣⊱  ❌ *CALL & VC*
+┣⊱  \`\`\`Melanggar??\`\`\` *Banned + Out Group*
+┬
+╰────────────────────────
+
+
+╭──────「 *ABOUT* 」
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}report lapor bug*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix} info*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}donasi*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}owner*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}speed*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}daftar*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}limit*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}totaluser*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}blocklist*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}banlist*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}premiumlist*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}bahasa*
+┬
+╰────────────────────────
+❍━━━━━━━━━━━━━━━━━━❍
+❍【♡ۣۜۜ፝͜͜͡͡✿➣ *DONASI*
+❍━━━━━━━━━━━━━━━━━━❍
+*Sekali² Donasi Lah bwang Jan Make Bot Mulu Kgk Donasi*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *Pulsa Axis 083144780782*
+
+
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭──────「 *MEDIA DOWNLOADER* 」
+┴
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tiktokstalk [username]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}igstalk [username]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}instavid link valid*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}instaimg link valid*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}instastory [username]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}ssweb url*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}url2img Url*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tiktok*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}fototiktok*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}kbbi*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}wait*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}trendtwit*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}google berita terkini*
+┬
+╰────────────────────────
+
+
+╭──────「 *CREATOR MENU* 」
+┴
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}quotemaker tx/wtrmk/tema*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nulis nama/kelas/text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}croman text dan text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}slide text BOT text*
+├───────────────────
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tp 1 - 162*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}ep 1 - 216*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tahta Zaki*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}cglass text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}cstyle text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}cgame text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}clove text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}cparty text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}csky text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tts id Haii*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}ttp text*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}cpaper text*
+├───────────────────
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}stiker*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}gifstiker*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}toimg*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}img2url*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tomp3*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}ocr*
+┬
+╰──────────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}modeanime On/Off*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}neonime naruto*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}naruto*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}minato*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}boruto*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}hinata*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sakura*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sasuke*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}toukachan*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}rize*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}akira*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}itori*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}kurumi*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}miku*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}anime*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}animecry*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}animekiss*
+┬
+╰───────────────────────
+
+
+╭───────「 *GROUP ONLY* 」
+┴
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}antilink On/Off*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}welcome On/Off*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}grup buka/tutup*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}ownergrup*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}setpp*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}infogc*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}add 628xxxxxxxxxx*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}kick @mentioned*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}kicktime @mentioned*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}promote @mentioned*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}demote @mentioned*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}setname*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}setdesc*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}linkgrup*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tagme*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}hidetag*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}tagall*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}mentionall*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}fitnah @mentioned/isi/balasan*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}listadmin*
+┬
+╰────────────────────────
+
+
+╭───────「 *GROUP ONLY ADMIN* 」
+┴
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nsfw On/Off*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nsfwloli*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nsfwblowjob*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nsfwneko*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}nsfwtrap*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}hentai* [DOSA TANGGUNG SENDIRI]
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}simih On/Off*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}bott on/off*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}anjing*
+│➻ *${prefix}kucing*
+│➻ *${prefix}testime*
+│➻ *${prefix}hilih*
+│➻ *${prefix}apakah*
+│➻ *${prefix}kapankah*
+│➻ *${prefix}bisakah*
+│➻ *${prefix}rate*
+│➻ *${prefix}watak*
+│➻ *${prefix}hobby*
+│➻ *${prefix}infogempa*
+│➻ *${prefix}infonomor*
+│➻ *${prefix}quotes*
+│➻ *${prefix}truth*
+│➻ *${prefix}dare*
+│➻ *${prefix}katabijak*
+│➻ *${prefix}fakta*
+│➻ *${prefix}darkjokes*
+│➻ *${prefix}bucin*
+│➻ *${prefix}pantun*
+│➻ *${prefix}katacinta*
+│➻ *${prefix}jadwaltvnow*
+│➻ *${prefix}hekerbucin*
+│➻ *${prefix}katailham*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jarak Jombang/Nama Kota*
+│➻ *${prefix}translate en/Apa kabar?*
+│➻ *${prefix}pasangan nama lu/doi ly*
+│➻ *${prefix}gantengcek text*
+│➻ *${prefix}cantikcek text*
+│➻ *${prefix}artinama nama lu*
+│➻ *${prefix}persengay Topan*
+│➻ *${prefix}pbucin text*
+│➻ *${prefix}bpfont text*
+│➻ *${prefix}textstyle text*
+│➻ *${prefix}jadwaltv antv*
+│➻ *${prefix}lirik nama lagu*
+│➻ *${prefix}chord nama lagu*
+│➻ *${prefix}wiki Adolf Hitler*
+│➻ *${prefix}brainly pertanyaan*
+│➻ *${prefix}resepmasakan rawon*
+│➻ *${prefix}map nama kota*
+│➻ *${prefix}film Fast and Farious*
+│➻ *${prefix}pinterest gambar kucing*
+│➻ *${prefix}infocuaca nama kota*
+│➻ *${prefix}jamdunia nama kota*
+│➻ *${prefix}mimpi Ular*
+│➻ *${prefix}infoalamat jalan jombang*
+│➻ *${prefix}playstore WhatsApp*
+┬
+╰───────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}readmore*
+│➻ *${prefix}puisiimg*
+│➻ *${prefix}asupan*
+│➻ *${prefix}tebakgambar*
+│➻ *${prefix}caklontong*
+│➻ *${prefix}family100*
+│➻ *${prefix}memeindo*
+│➻ *${prefix}kalkulator 13*12*
+│➻ *${prefix}moddroid lightroom*
+│➻ *${prefix}happymod lightroom*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}randomkpop*
+│➻ *${prefix}cersex*
+│➻ *${prefix}randombokep*
+│➻ *${prefix}pornhub stepMoms*
+│➻ *${prefix}xvideos japan*
+│➻ *${prefix}nekopoi oni chichi*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}jadwalsholat nama kota*
+│➻ *${prefix}quran*
+│➻ *${prefix}quranlist*
+│➻ *${prefix}quransurah 1*
+┬
+╰────────────────────────
+
+
+╭──────「 *FIND ME SIR* 」
+┴
+│➻ *${prefix}becrypt string*
+│➻ *${prefix}encode64 string*
+│➻ *${prefix}decode64 encrypt*
+│➻ *${prefix}encode32 string*
+│➻ *${prefix}decode32 encrypt*
+│➻ *${prefix}encbinary string*
+│➻ *${prefix}decbinary encrypt*
+│➻ *${prefix}encoctal string*
+│➻ *${prefix}decoctal encrypt*
+│➻ *${prefix}hashidentifier Encrypt Hash*
+│➻ *${prefix}dorking dork*
+│➻ *${prefix}pastebin teks*
+│➻ *${prefix}tinyurl link*
+│➻ *${prefix}bitly link*
+┬
+╰────────────────────────
+
+
+╭──────「 *OTHERS FUN & GAME* 」
+┴
+│➻ *${prefix}spamcall 083xxxxxxxxx*
+│➻ *${prefix}spamgmail contoh@gmail.com*
+┬
+╰────────────────────────
+
+
+╭─────────「 *OWNER ONLY* 」
+┴
+│➻ *${prefix}addprem mentioned*
+│➻ *${prefix}removeprem mention*
+│➻ *${prefix}setmemlimit*
+│➻ *${prefix}setlimit*
+│➻ *${prefix}setreply*
+│➻ *${prefix}setprefix*
+│➻ *${prefix}setnamebot*
+│➻ *${prefix}setppbot*
+│➻ *${prefix}bc*
+│➻ *${prefix}bcgc*
+│➻ *${prefix}ban*
+│➻ *${prefix}unban*
+│➻ *${prefix}block*
+│➻ *${prefix}unblock*
+│➻ *${prefix}clearall*
+│➻ *${prefix}delete*
+│➻ *${prefix}clone*
+│➻ *${prefix}getses*
+│➻ *${prefix}leave*
+┬
+╰────────────────────────
+
+
+╭────────「 *PREMIUM ONLY* 」
+┴
+│➻ *${prefix}playmp3 menepi*
+│➻ *${prefix}fb link video*
+│➻ *${prefix}snack link snack video*
+│➻ *${prefix}ytmp3 link yt*
+│➻ *${prefix}ytmp4 link yt*
+│➻ *${prefix}joox Monolog Pamungkas*
+│➻ *${prefix}smule Link Video Smule*
+┬
+╰────────────────────────
+❍━━━━━━━━━━━━━━━━━━❍
+❍【♡ۣۜۜ፝͜͜͡͡✿➣ *NEW FITUR [PERBAIKAN]*
+❍━━━━━━━━━━━━━━━━━━❍
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}iri*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}pale [EROR]*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}dj1*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}dj2*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound1*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound2*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound3*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound4*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound5*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound6*
+├ ♡ۣۜۜ፝͜͜͡͡✿➤ *${prefix}sound7*
+
+
+
+╭─────「 *THANKS TO* 」
+┴
+│➲ *SadBoy Team*
+│➲ *M.ZakiGans
+┬
+╰────────────────────────`
+}
+
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
+
+// info bot 
+const bottt = (prefix) => {
+return `
+\`\`\`Untuk Sekarang Bot Hanya Bisa Digunakan Di Group Karna\`\`\` *KUOTA MAHAL*\n\n *Mohon Pengertiannya*
+Sekali Lagi Maaf
+
+Jika Bot Ini Ada Di Grup Anda Admin Grup Suruh Aktifkan Bot Dengan Cara ${prefix}bott aktif
+`
+}
+exports.bottt = bottt
+// donasi menu 
+const donasi = (name) => { 
+	return `       
+╭─────「 *DONASI SEIKHLASNYA* 」
+┴
+│√ *PULSA: 083144780782*
+│√ *OVO : 08311800241*
+│
+│\`\`\`Pulsa\`\`\`
+│ *15k Invite Bot Ke Grup selama 1 bulan*
+│
+│\`\`\`OVO\`\`\`
+│ *10k Invite Bot Ke Group Selama 1 Bulan*
+│ *Tutor Buat Bot Sendiri? 5k
+┬
+╰──────「 *ZakiGans* 」
+
+Untuk Kelangsungan Hidup Bot Karna Kuota Mahal:'
+`
+}
+exports.donasi = donasi
+
+// bahasa list
+const bahasa = (prefix) => {
+return `
+List Bahasa Untuk Command *${prefix}tts*
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
+
+// Limit
+const limitend = (pushname2) => {
+        return`*maaf ${pushname2} limit hari ini habis*\n*limit di reset setiap jam 12:00 WIB TENGAH MALAM*`
+}
+
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: ${limitCounts}
+`
+}
+
+exports.limitend = limitend
+exports.limitcount = limitcount
